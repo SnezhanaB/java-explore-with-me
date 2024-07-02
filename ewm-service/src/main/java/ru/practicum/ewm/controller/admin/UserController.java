8b27b99ec6bb34f2f9635468deab3e35652c0a18
Brel Snezhana <brelok@1-pp.ru>
2024-07-02 23:56:10 +0300

    add UserController

--- ewm-service/src/main/java/ru/practicum/ewm/controller/admin/UserController.java
+++ ewm-service/src/main/java/ru/practicum/ewm/controller/admin/UserController.java
@@ -2,7 +2,6 @@ package ru.practicum.ewm.controller.admin;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
