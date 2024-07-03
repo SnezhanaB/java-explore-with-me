9c5624024c3d8e64d4dde8fa9073e6f15dfd88a9
Brel Snezhana <brelok@1-pp.ru>
2024-07-03 18:48:23 +0300

    add PublicCategoryController

---
+++ ewm-service/src/main/java/ru/practicum/ewm/controller/pub/PublicCategoryController.java
@@ -0,0 +1,53 @@
+package ru.practicum.ewm.controller.pub;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.ewm.dto.CategoryDto;
+
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.PositiveOrZero;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Public: Категории
+ * <p>
+ * Публичный API для работы с категориями
+ */
+@Slf4j
+@Validated
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(path = "/categories")
+public class PublicCategoryController {
+
+    /**
+     * Получение категорий
+     * @param from количество категорий, которые нужно пропустить для формирования текущего набора
+     * @param size количество категорий в наборе
+     * @return список категорий
+     * <p>
+     * В случае, если по заданным фильтрам не найдено ни одной категории, возвращает пустой список
+     */
+    @GetMapping
+    public List<CategoryDto> getCategories(@RequestParam(defaultValue = "0") @PositiveOrZero Integer from,
+                                           @RequestParam(defaultValue = "10") @Positive Integer size) {
+        log.info("[GET /categories] получение списка категорий");
+        // TODO
+        return Collections.emptyList();
+    }
+
+    /**
+     * Получение информации о категории по её идентификатору
+     * @param catId идентификатор категории
+     * @return категория
+     */
+    @GetMapping("/{catId}")
+    public CategoryDto getCategory(@PathVariable Long catId) {
+        log.info("[GET /categories/{}] получение информации о категории по её идентификатору", catId);
+        // TODO
+        return null;
+    }
+}
