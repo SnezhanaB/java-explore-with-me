017cf6e97062ec65a0008f04eb5526129a1fc89e
Brel Snezhana <brelok@1-pp.ru>
2024-07-16 20:23:43 +0300

    fix query

--- ewm-service/src/main/java/ru/practicum/ewm/repository/CommentRepository.java
+++ ewm-service/src/main/java/ru/practicum/ewm/repository/CommentRepository.java
@@ -12,7 +12,7 @@ import java.util.List;
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     @Query("select c " +
             "from comments as c " +
-            "where lower(c.text) like lower(concat('%', ?1, '%') ) and c.eventId in ?2")
+            "where lower(c.text) like lower(concat('%', ?1, '%') ) and c.event.id in (?2)")
     Page<Comment> search(String text, List<Long> eventIds, Pageable pageable);
 
     Page<Comment> findAllByEventId(Long eventId, Pageable pageable);
